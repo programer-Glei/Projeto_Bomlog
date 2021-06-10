bb53f1b363855967ae401b04c8a42c3bcc2eed5c
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-06-09 23:48:35 -0300

    proximo comit

--- java/javascript.js
+++ java/javascript.js
@@ -1,1 +1,23 @@
 
+
+function selectImage(event) {
+    const button = event.currentTarget
+    console.log(button)
+    //removar todas as classes .active
+    const buttons = document.querySelectorAll(".op .op-item")
+    console.log(buttons)
+    buttons.forEach((button) => {
+        button.classList.remove("active")
+    })
+    // selecionar a image clicada
+    const image = button.children[0][0]
+    console.log(image)
+    const imageContainer = document.querySelector(".op-image img")
+    console.log(imageContainer)
+
+    //atualizar o container de image
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add("active")
+}
